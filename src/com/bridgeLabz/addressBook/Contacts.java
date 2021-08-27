3cab3d30b08c42f3e05f111448aaf86058f79eb9
ajaybarath <ajaybarath3@gmail.com>
2021-08-27 15:56:14 +0530

    ADDED: class created to store contact details

--- src/com/bridgeLabz/addressBook/Contacts.java
+++ src/com/bridgeLabz/addressBook/Contacts.java
@@ -2,4 +2,27 @@ package com.bridgeLabz.addressBook;
 
 public class Contacts {
 
+	String firstName;
+	String lastName;
+	String city;
+	String state;
+	String address;
+	String zip;
+	String phoneNumber;
+
+	public Contacts(String firstName, String lastName, String city, String state, String address, String zip,
+			String phoneNumber) {
+		
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.city = city;
+		this.state = state;
+		this.address = address;
+		this.zip = zip;
+		this.phoneNumber = phoneNumber;
+
+	}
+	
+	
+
 }
