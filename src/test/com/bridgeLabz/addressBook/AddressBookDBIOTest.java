69e2a714dc77bf4af137534670e3535659f08115
ajaybarath <ajaybarath3@gmail.com>
2021-09-28 18:17:39 +0530

    Added: function to test getting all data from db

--- src/test/com/bridgeLabz/addressBook/AddressBookDBIOTest.java
+++ src/test/com/bridgeLabz/addressBook/AddressBookDBIOTest.java
@@ -1,2 +1,22 @@
-package test.com.bridgeLabz.addressBook;public class AddressBookDBIOTest {
+package test.com.bridgeLabz.addressBook;
+
+import main.com.bridgeLabz.addressBook.AddressBook;
+import main.com.bridgeLabz.addressBook.AddressBookException;
+import main.com.bridgeLabz.addressBook.Contacts;
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.sql.SQLException;
+import java.util.List;
+
+public class AddressBookDBIOTest {
+
+    @Test
+    public void readDatabaseAndGetEntriesCount() throws SQLException, AddressBookException {
+
+        AddressBook employeePayRollService = new AddressBook();
+        List<Contacts> list = employeePayRollService.readAddressBookFromDB();
+        long entries = list.size();
+        Assert.assertEquals(6, entries);
+    }
 }
