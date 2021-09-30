5c93ab6319676edfeb1c3e6c77418d4829b836f3
ajaybarath <ajaybarath3@gmail.com>
2021-09-30 22:39:14 +0530

    ADDED: model class for relationship type table

--- src/main/com/bridgeLabz/addressBook/Relationship.java
+++ src/main/com/bridgeLabz/addressBook/Relationship.java
@@ -1,2 +1,31 @@
-package main.com.bridgeLabz.addressBook;public class Relationship {
+package main.com.bridgeLabz.addressBook;
+
+public class Relationship {
+
+    int id;
+    String name;
+
+    public Relationship(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public Relationship() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
