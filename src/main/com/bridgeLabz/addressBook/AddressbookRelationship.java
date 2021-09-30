f417f5d8c94ce59cb359c65e16a37d028f10ae1d
ajaybarath <ajaybarath3@gmail.com>
2021-09-30 22:38:49 +0530

    ADDED: model class for address relationship table

--- src/main/com/bridgeLabz/addressBook/AddressbookRelationship.java
+++ src/main/com/bridgeLabz/addressBook/AddressbookRelationship.java
@@ -1,2 +1,32 @@
-package main.com.bridgeLabz.addressBook;public class AddressbookRelationShip {
+package main.com.bridgeLabz.addressBook;
+
+public class AddressbookRelationship {
+
+    int contactId;
+
+    int relationshipId;
+
+    public AddressbookRelationship(int contactId, int relationshipId) {
+        this.contactId = contactId;
+        this.relationshipId = relationshipId;
+    }
+
+    public AddressbookRelationship() {
+    }
+
+    public int getContactId() {
+        return contactId;
+    }
+
+    public void setContactId(int contactId) {
+        this.contactId = contactId;
+    }
+
+    public int getRelationshipId() {
+        return relationshipId;
+    }
+
+    public void setRelationshipId(int relationshipId) {
+        this.relationshipId = relationshipId;
+    }
 }
